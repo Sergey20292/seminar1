551c023d4442127d7d99cc33f344746cb5f820bb
Sergey <sergeylibanovsky@yandex.ru>
2022-12-08 14:42:59 +0300

    DZ2

--- DZ1.cs
+++ DZ1.cs
@@ -1,100 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-//onsole.WriteLine("Hello, World!");
-
-// задача. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
+﻿
+//   задача.Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 /*
-Console.WriteLine("введите первое число");
-string num1 = Console.ReadLine()!;
-int num1int = int.Parse(num1);
-
-Console.WriteLine("Введите второе чсило");
-string num2 = Console.ReadLine()!;
-int num2int = int.Parse(num2);
-
-if (num1int > num2int)
-
+Console.WriteLine("Введите трехзначное число");
+int num1 = int.Parse(Console.ReadLine()!);
+if ((num1 >= 100) && (num1 < 1000))
 {
-    Console.WriteLine(num1int + "Больше" + num2int);
-}
-else if (num2int > num1int)
-{
-  Console.WriteLine(num2int + "Больше" + num1int);
-}
+    int firstDigit = num1 % 100;
+    int secondDigit = firstDigit / 10;
+    Console.WriteLine(secondDigit);
 
+}
 else
 {
-   Console.WriteLine("числа равны");
-}
-
-
-//задача. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-
-Console.WriteLine("Выедите целое число");
-string num1 = Console.ReadLine()!;
-int num1int = int.Parse(num1);
-
-if (num1int % 2 == 0)
-{
-    Console.WriteLine(num1int + "Число четное");
-}
-
-else
-//{
-//    Console.WriteLine(num1int + "Число не четное ");
-//}
-
-//задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
-//Console.WriteLine("ведите первое число") ;
-string num1 = Console.ReadLine()!;
-int num1int= int.Parse(num1);
-
-
-Console.WriteLine("ведите второе число") ;
-string num2 = Console.ReadLine()!;
-int num2int= int.Parse(num2);
-
-Console.WriteLine("ведите третье число") ;
-string num3 = Console.ReadLine()!;
-int num3int= int.Parse(num3);
-
-if(num1int>num2int)
-{
-    if(num1int>num3int)
-    {
-        Console.WriteLine(num1int + "самое большое число");
-
-   }
-   else
-    {
-        Console.WriteLine(num3int +"самое большое число");
-    }
-}else
-{
-   if (num2int > num3int)
- {
-     Console.WriteLine( num2int+ "самое большое число");
-
- }
-else
- {
-    Console.WriteLine( num3int + "самое большео число");
- }
-}
-
-//задача.Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа
-
-Console.WriteLine("введите целое число больше нууля");
-string num1 = Console.ReadLine()!;
-int num1int = int.Parse(num1);
-
-for (int i = 1; i < num1int; i++)
-
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(i + "целое число");
-    }
+    Console.WriteLine("Введено не трехзначное число");
 }
 */
+
+// задача.Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*
+Console.WriteLine("Введите номер дня недели(1-7)");
+int num1 = int.Parse(Console.ReadLine()!); 
+
+if (num1>0&& num1<8)
+
+{
+    if(num1==6 || num1 ==7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Будни");
+    }
+}
+else
+{
+    Console.WriteLine("число выходящие из диопазона 1 до 7 ");
+
+}
+*/
+
+
+int number = ReadInt("Введите число");
+int count = number.ToString()!.Length;
+Console.Write(MakeArray(number, count));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет,");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
+
 
